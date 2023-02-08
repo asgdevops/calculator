acac0949d4ebc12340edad3b9b3985decc5ba65d
Anonio Salazar <antonio.salazar.devops@gmail.com>
2023-02-07 22:33:15 -0600

    Calculator

---
+++ src/test/java/com/ansalaza/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansalaza.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
